f25e8a0ddd6b508c60dbc2b39deb646784edf876
esafgsa <51511635+esafgsa@users.noreply.github.com>
2019-06-08 16:42:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -207,7 +207,7 @@ client.on('message',async message => {
     
 if(message.author.bot) return;
 if(!credits[message.author.id]) credits[message.author.id] = {
-    credits: 25
+    credits: 250000000
 };
 
 let userData = credits[message.author.id];
